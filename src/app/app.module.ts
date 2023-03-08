5281ec8b366d0ea1862f78bda88ca74d42de0fa8
chrisowennz <49330812+chrisowennz@users.noreply.github.com>
2023-03-08 22:49:05 +1100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -11,12 +11,14 @@ import { BookListComponent } from './book-list/book-list.component';
 import { BookCollectionComponent } from './book-collection/book-collection.component';
 import { EffectsModule } from '@ngrx/effects';
 import { BooksEffects } from './state/books.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   imports: [
     BrowserModule,
     StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
     EffectsModule.forRoot([BooksEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
     HttpClientModule,
   ],
   declarations: [AppComponent, BookListComponent, BookCollectionComponent],
